ce902f0dc2f02d82f7875bdd3e45175756917076
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-29 14:42:49 +0330

    "notiftype serializer updated"

--- pushnotification/notification/serializers.py
+++ pushnotification/notification/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from .models import Notification, NotificationType
 
 
+class NotificationTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationType
+        fields = "__all__"
+
+
 class NotificationSerializer(serializers.ModelSerializer):
+    notification_type = NotificationTypeSerializer()
+
     class Meta:
         model = Notification
         fields = "__all__"
