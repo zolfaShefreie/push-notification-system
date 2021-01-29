7326570a58a7667a31f9f5112fee2e26069e75e8
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-29 04:11:14 +0330

    "Server added"

---
+++ pushnotification/notification/server.py
@@ -0,0 +1,94 @@
+import socket
+import select
+import time
+from .models import Notification
+from .serializers import NotificationSerializer
+
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+token = [1, 2, 3, 4]
+
+notifs = {}
+
+# List of sockets for select.select()
+sockets_list = [server_socket]
+
+# List of connected clients - socket as a key, user header and token as data
+clients = {}
+
+print(f'Listening for connections on {IP}:{PORT}...')
+
+
+def receive_message(client_socket):
+
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+        if not len(message_header):    # If we received no data, client gracefully closed a connection
+            return False
+        message_length = int(message_header.decode('utf-8').strip())
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+        return False
+
+
+while True:
+
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    # Iterate over notified sockets
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = receive_message(client_socket)
+
+            # If False - client disconnected before he sent token
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+
+            print('Accepted new connection from {}:{}, token: {}'.format(*client_address, user['data'].decode('utf-8')))
+
+        # Else existing socket is sending a message
+        else:
+            user = clients[notified_socket]
+            t = user['data'].decode('utf-8')
+
+            # XXX FILTER WILL CHANGE XXX
+            notifications = Notification.objects.filter(receiver__id__exact=t)
+            notif_ser = NotificationSerializer(notifs)
+            notifs = notif_ser.data
+
+            if notifs:
+                message = notifs
+                # if message is False:
+                #     print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                #     sockets_list.remove(notified_socket)
+                #     del clients[notified_socket]
+                #     continue
+                user = clients[notified_socket]
+                print(f'Notifications send to {t}: {message}')
+                hm = (str(message))
+                notified_socket.sendall(hm.encode())
+                Notification.objects.filter(receiver__id__exact=t).delete()
+                notifs = {}
+                break
+            else:
+                time.sleep(5)
+                notifications = Notification.objects.filter(receiver__id__exact=t)
+
+    # handle some socket exceptions just in case
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
